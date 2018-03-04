7644d37f86601dedd50ffa576a87ddbc0bcee7a6
darcycervh <darcycervera@gmail.com>
2018-03-04 13:09:24 -0500

    agregando imagenes al dropdown

--- js/principal.js
+++ js/principal.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
+  // botón de registro
   var $signup = $('#signup');
   $signup.on('click', function() {
     window.location.href = '../views/signup.html';
   });
+  // botón de inicio de sesión
   var $login = $('#login');
   $login.on('click', function() {
     window.location.href = '../views/login.html';
   });
 });
-
-/*
-setTimeout(function(){ 
-    window.location.href="views/principal.html"; 
-    }, 5000)
-*/
